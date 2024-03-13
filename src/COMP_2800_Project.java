d31e4137eb1e97878f0362ace89d8d84ca26e823
mantecademani <pbinspanish@outlook.com>
2024-03-13 15:23:23 -0400

    implement basic render and game loop

--- src/COMP_2800_Project.java
+++ src/COMP_2800_Project.java
@@ -1,5 +1,29 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class COMP_2800_Project {
     public static void main(String[] args) throws Exception {
-        
+        // avoid frame flicker on manual resize
+        System.setProperty("sun.awt.noerasebackground", "true");
+
+        // Variables
+        String appTitle = "COMP-2800 Game Project";
+        JFrame frame = new JFrame(appTitle); // initialize a frame
+        frame.setBackground(Color.black);
+
+        // Frame Configuration
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(GameCanvas.GAME_WIDTH, GameCanvas.GAME_HEIGHT);
+        frame.setLocationRelativeTo(null);
+
+        // GameCanvas
+        GameCanvas gameCanvas = new GameCanvas();
+        frame.add(gameCanvas);
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        gameCanvas.start();
     }
 }
---
+++ src/GameCanvas.java
@@ -0,0 +1,88 @@
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+
+import java.util.Random;
+
+public class GameCanvas extends Canvas implements Runnable, KeyListener {
+	// Core
+	private Thread thread;
+	private BufferStrategy bs;
+
+	public static final int GAME_WIDTH = 960, GAME_HEIGHT = 540;
+
+	private Random rnd = new Random();
+
+	public GameCanvas() {
+		setFocusable(true);
+		this.addKeyListener(this);
+	}
+
+	public void start() {
+		this.requestFocus();
+		this.createBufferStrategy(2);
+		bs = this.getBufferStrategy();
+		thread = new Thread(this, "Game Thread");
+		thread.start();
+	}
+
+	@Override
+	public void run() {
+		// Setup Ticking
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		double delta = 0;
+
+		final int UPS_CAP = 60;
+
+		while (true) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / (double) (1000000000 / UPS_CAP);
+			lastTime = now;
+
+			while (delta >= 1) {
+				tick();
+				delta--;
+			}
+
+			render();
+
+			if (System.currentTimeMillis() - timer > 1000)
+				timer += 1000;
+		}
+	}
+
+	private void tick() {
+		// game logic
+	}
+
+	public void render() {
+		// rendering
+
+		// test background
+		Graphics2D background = (Graphics2D) bs.getDrawGraphics();
+		background.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		background.setColor(Color.CYAN);
+		background.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+		background.dispose();
+
+
+		// show the buffer
+		bs.show();
+	}
+
+	// Input Handling
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
+		
+	}
+}
